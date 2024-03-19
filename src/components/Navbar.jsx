370471412ba9626d6dfcfbf46896c8a0532e215d
Siri Høyås <siri@skjervold.me>
2024-03-19 10:09:17 +0100

    Refactor Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import Typography from "@mui/material/Typography";
 
 function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <AppBar position="static">
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
